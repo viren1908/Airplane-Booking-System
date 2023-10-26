368358963c4397efa7ac0a9373eeae17a5424a8e
Viren Wanekar <viren.wanekar@gmail.com>
2023-10-26 23:59:54 +0530

    Basic Project and Services Setup

---
+++ src/routes/v1/airplane-routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const { AirplaneController } = require('../../controllers');
+const { AirplaneMiddlewares } = require('../../middlewares');
+
+const router = express.Router();
+
+// /api/v1/airplanes POST
+router.post('/', 
+        AirplaneMiddlewares.validateCreateRequest,
+        AirplaneController.createAirplane);
+
+// /api/v1/airplanes GET
+router.get('/', 
+        AirplaneController.getAirplanes);
+
+// /api/v1/airplanes/:id GET
+router.get('/:id', 
+        AirplaneController.getAirplane);
+
+// /api/v1/airplanes/:id DELETE
+router.delete('/:id', 
+        AirplaneController.destroyAirplane);
+
+router.patch('/:id', 
+        AirplaneController.updateAirplane);
+
+module.exports = router;
